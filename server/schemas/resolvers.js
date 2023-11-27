aa0759a27b52c17213e34cd52767c2730bcae847
lilpinc <133043171+lilpinc@users.noreply.github.com>
2023-11-26 21:50:15 -0500

    add outline of index.js, and typedefs.js under schemas. still needs a lot of information. same with resolveres

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,14 @@
+const { Activity, Client, Employee, Financial, Payer, Program, Schedule } = require('../models');
+
+
+const resolvers = {
+    Query: {
+        
+      },
+      Mutation: {
+        
+}}
+
+
+
+module.exports = resolvers;
