651badc8d574f93180d846dc8ed3f94654b70be7
Jessie Shockey <shockeyjessie92@gmail.com>
2023-12-04 12:59:17 -0700

    Create User Model

--- server/models/index.js
+++ server/models/index.js
@@ -5,6 +5,7 @@ const Financial = require('./Financial');
 const Employee = require('./Employee');
 const Client = require('./Client');
 const Activity = require('./Activity');
+const User = require('./User');
 
 
-module.exports = {Schedule, Program, Payer, Financial, Employee, Client, Activity };
+module.exports = {Schedule, Program, Payer, Financial, Employee, Client, Activity, User };
