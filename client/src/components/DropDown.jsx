0f60b08754c855a6170cc602bd770f54586e75d0
lilpinc <133043171+lilpinc@users.noreply.github.com>
2023-12-07 21:36:43 -0500

    add navigation and dropdown components

---
+++ client/src/components/DropDown.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link, useLocation } from 'react-router-dom';
+
+const DropdownMenu = () => {
+
+    const currentPage = useLocation().pathname;
+
+    return (
+        <div className="dropdown-menu">
+            <ul>
+                <li className="dropdown-item">
+                    <Link
+                        to="/PayerAdministration"
+                        // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                        className={currentPage === '/PayerAdministration' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Payer Administration
+                    </Link>
+                </li>
+                <li className="dropdown-item">
+                    <Link
+                        to="/ActivityAdministration"
+                        // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                        className={currentPage === '/ActivityAdministration' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Activity Administration
+                    </Link>
+                </li>
+                <li className="dropdown-item">
+                    <Link
+                        to="/ProgramAdministration"
+                        // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                        className={currentPage === '/ProgramAdministration' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Program Administration
+                    </Link>
+                </li>
+                <li className="dropdown-item">
+                    <Link
+                        to="/EmployeeAdministration"
+                        // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+                        className={currentPage === '/EmployeeAdministration' ? 'nav-link active' : 'nav-link'}
+                    >
+                        Employee Administration
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default DropdownMenu;
+
+
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,26 +1,61 @@
-import React, {useState} from "react";
-import { FaSearch } from "react-icons/fa";
+// import React, { useState, useEffect } from "react";
+// // import { FaSearch } from "react-icons/fa";
+// import {QUERY_CLIENT} from '../graphql/queries/'
+// import {QUERY_CLIENTS} from '../graphql/queries/'
+// import {useNavigate} from 'react-router-dom';
+// import {useQuery} from '@apollo/client';
 
-export const SearchBar = () => {
-    const [searchInput, setSearchInput] = useState("")
 
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-      };
-      
-      if (searchInput.length > 0) {
-          countries.filter((country) => {
-          return country.name.match(searchInput);
-      });
-      }
+// export const SearchBar = () => {
+//   const {loading, data } = useQuery(QUERY_CLIENTS);
+//   const clients = data?.demographics.name || [];
 
-    return (
-        < div className="input-wrapper">
-            <FaSearch id="search-icon"/>
-            <input placeholder="Search" value={input} 
-            onChange = {(e) => handleChange(e.target.value)}/>
-    </div >
-    );
-};
+//   // fetch data (how do we fetch the info?) query client?
+
+//   // useEffect(() => {
+//   //   fetch()
+//   //   .then(res => res.json())
+//   //   .then(data => {
+//   //     setFilterSearch(search);
+
+//   //   })
+//   //   .catch(err => console.log(err));
+//   // }, [])
+
+// // set useState
+//   const [search, setSearch] = useState([])
+//   const [filterSearch, setFilterSearch] = useState([])
+ 
+
+//   // filter results as the user types out client name
+//   const handleFilter = (value) => {
+//     const res = filterSearch.filter(f => f.name.toLowerCase().includes(value))
+//     setSearch(res);
+//     if (value === "") {
+//       setSearch([]);
+//     }
+//   };
+
+//   const navigate = useNavigate();
+
+//   const navigateToClient = (value) => {
+//     //navigate to /client via id param
+//     // if name matches a client name use the correlating id to pull up the client page?
+//     navigate('');
+//   }
+//   // create onclick function to redirect to clients information page
+
+//     return (
+//       <div className="search">
+//         <div className="searchInputs">
+//             <input type="search" placeholder="Enter Client Name"  onChange={e => handleFilter(e.target.value)}/>
+//             <button><FaSearch id="searchIcon"/></button>
+//         </div>
+//         <ul>
+//           {/* create an onclick function */}
+//             {data.map((search, index) => <h1 key={index} onClick={e => navigateToClient(e.target.value)}>{search.name}</h1>)}
+//         </ul>
+//       </div>
+//     );
+// };
